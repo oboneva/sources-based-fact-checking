7ba32bba3c4f4dce6717cc262209d76bb9c1af98
Ognyanka Boneva <ogi.boneva@gmail.com>
2022-04-25 18:57:23 +0300

    Clean text data

--- preprocess_utils.py
+++ preprocess_utils.py
@@ -2,6 +2,7 @@ import json
 from os import walk
 
 import dateparser
+from unidecode import unidecode
 
 
 def clean_access_dates(articles_dir: str):
@@ -44,8 +45,104 @@ def clean_access_dates(articles_dir: str):
             json.dump(data, outfile, indent=4)
 
 
+def clean_newlines(articles_dir: str):
+    files = []
+    for (dirpath, dirnames, filenames) in walk(articles_dir):
+        files.extend(filenames)
+        break
+
+    for article in files:
+        if article == ".DS_Store":
+            continue
+
+        with open(f"{articles_dir}/{article}") as f:
+            data = json.load(f)
+
+        for source in data["sources"]:
+            text_to_clean = (
+                source["text_cleaned"] if source["text_cleaned"] else source["text"]
+            )
+            text_to_clean = text_to_clean.strip()
+
+            text_to_clean = text_to_clean.replace("\n\n,", ",")
+            text_to_clean = text_to_clean.replace("\n \n,", ",")
+
+            text_to_clean = text_to_clean.replace(",\n\n", ", ")
+            text_to_clean = text_to_clean.replace(",\n \n", ", ")
+
+            text_to_clean = text_to_clean.replace(" \n\n", " ")
+            text_to_clean = text_to_clean.replace("\n\n ", " ")
+
+            text_to_clean = text_to_clean.replace("\n\n", " ")
+
+            text_to_clean = text_to_clean.replace(" \n", " ")
+            text_to_clean = text_to_clean.replace("\n ", " ")
+            text_to_clean = text_to_clean.replace("\n", " ")
+
+            source["text_cleaned"] = text_to_clean
+
+            for link in source["links"]:
+                text_link_to_clean = link["link_text"]
+
+                text_link_to_clean = text_link_to_clean.strip()
+
+                text_link_to_clean = text_link_to_clean.replace(" \n", " ")
+                text_link_to_clean = text_link_to_clean.replace("\n ", " ")
+                text_link_to_clean = text_link_to_clean.replace("\n", " ")
+
+                link["link_text"] = text_link_to_clean
+
+        with open(f"{articles_dir}/{article}", "w") as outfile:
+            json.dump(data, outfile, indent=4)
+
+
+def clean_empty_links(articles_dir: str):
+    files = []
+    for (dirpath, dirnames, filenames) in walk(articles_dir):
+        files.extend(filenames)
+        break
+
+    for article in files:
+        if article == ".DS_Store":
+            continue
+
+        with open(f"{articles_dir}/{article}") as f:
+            data = json.load(f)
+
+        for source in data["sources"]:
+            if (
+                len(source["links"]) == 1
+                and source["links"][0]["link"] == ""
+                and source["links"][0]["link_text"] == ""
+                and source["links"][0]["domain"] == ""
+            ):
+                source["links"] = []
+
+        with open(f"{articles_dir}/{article}", "w") as outfile:
+            json.dump(data, outfile, indent=4)
+
+
+def clean_title(articles_dir: str):
+    files = []
+    for (dirpath, dirnames, filenames) in walk(articles_dir):
+        files.extend(filenames)
+        break
+
+    for article in files:
+        if article == ".DS_Store":
+            continue
+
+        with open(f"{articles_dir}/{article}") as f:
+            data = json.load(f)
+
+        data["title"] = unidecode(data["title"].strip())
+
+        with open(f"{articles_dir}/{article}", "w") as outfile:
+            json.dump(data, outfile, indent=4)
+
+
 def main():
-    clean_access_dates(articles_dir="./data/articles_parsed_clean_date")
+    clean_title(articles_dir="./data/articles_parsed_clean_date")
 
 
 if __name__ == "__main__":
